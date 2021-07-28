dd40e1df80420268067ade0dcdc7c616ed3bfbf6
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-07-28 17:43:53 +0200

    Install and configurate eslint

--- thn-test.js
+++ thn-test.js
@@ -1,7 +1,12 @@
-const data = new Object();
+const data = {};
 
-function getData(){
-
+function getDates() {
+  data.checkInDate = document.querySelector('#fb-qs-summary-dates-arrival > span').getAttribute('data-date');
+  data.checkOutDate = document.querySelector('#fb-qs-summary-dates-departure > span').getAttribute('data-date');
 }
 
-document.location.host === "www.book-secure.com"? getData() : 'error';
+function getData() {
+  getDates();
+}
+
+document.location.host === 'www.book-secure.com' ? getData() : 'error';
